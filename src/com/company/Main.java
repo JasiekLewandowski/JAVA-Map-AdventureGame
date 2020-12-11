a8a8c2502d4ca6b1412cff6771fd1dd1de6bf688
JasiekLewandowski <66948162+JasiekLewandowski@users.noreply.github.com>
2020-12-11 10:31:42 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,10 @@ public class Main {
     public static Map<Integer, Location> locations = new HashMap<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        locations.put(0, new Location(0, "Jesteś w domku przed komputerkiem."));
+        locations.put(0, new Location(0, "Jesteś w domu."));
         locations.put(1, new Location(1, "Jesteś w przedpokoju, zakładasz buty."));
-        locations.put(2, new Location(2, "Jesteś na klatce. Wyszedłeś z domu."));
-        locations.put(3, new Location(3, "Jesteś w windzie. Jedziesz."));
+        locations.put(2, new Location(2, "Jesteś na klatce, wyszedłeś z domu."));
+        locations.put(3, new Location(3, "Jesteś w windzie."));
         locations.put(4, new Location(4, "Jesteś przy recepcji."));
         locations.put(5, new Location(5, "Jesteś na ulicy."));
 
